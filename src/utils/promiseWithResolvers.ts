1b2191dade5290c2152d1b3e7ae3faec93d49e27
Kristoffer Brabrand <kristoffer@brabrand.no>
2026-01-28 23:11:41 +0100

    fix: bind withResolvers to Promise if it exist

--- src/utils/promiseWithResolvers.ts
+++ src/utils/promiseWithResolvers.ts
@@ -1,6 +1,6 @@
 export const promiseWithResolvers =
   // @ts-expect-error TODO: replace with `Promise.withResolvers()` once it lands in node
-  Promise.withResolvers ||
+  Promise.withResolvers?.bind(Promise) ||
   function promiseWithResolvers<T>() {
     let resolve!: (t: T) => void
     let reject!: (err: unknown) => void
