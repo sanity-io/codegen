542781acb20698e72da227cfd6c0eaa7b8a68d34
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-07-15 14:41:59 -0700

    feat!: remove bundled typegen CLI command, bin and oclif config

--- src/utils/percent.ts
+++
@@ -1,7 +0,0 @@
-const formatter = new Intl.NumberFormat('en-US', {
-  maximumFractionDigits: 1,
-  minimumFractionDigits: 1,
-  style: 'percent',
-})
-
-export const percent = (value: number): string => formatter.format(Math.min(value, 1))
